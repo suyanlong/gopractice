e2b571d00e24a1eb527e53c55c9b78dd9b4bc211
suyanlong <592938410@qq.com>
2018-02-18 15:10:03 +0800

    add routine test

---
+++ examples/routine.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"runtime"
+	"fmt"
+)
+
+func main() {
+	runtime.GOMAXPROCS(1)
+	go func() {
+		for i := 0; i < 100; i++ {
+			fmt.Println(i)
+		}
+	}()
+	// CPU 占用for循环。
+	//for {}
+	fmt.Println("hello world!")
+}
