b8f7a0cc7574c06903847f7b2ef8007230b49aa2
suyanlong <592938410@qq.com>
2018-02-26 15:53:40 +0800

    modify select.go file

--- examples/select.go
+++ examples/select.go
@@ -9,10 +9,13 @@ func main() {
 		select {
 		case <-ch1:
 			fmt.Println("ch1 pop one element")
+			ch2 <- 1
 		case <-ch2:
 			fmt.Println("ch2 pop one element")
+			ch1 <- 2
 		default:
 			fmt.Println("default")
+			ch1 <- 123
 		}
 	}
 
