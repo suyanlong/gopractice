3e576377b18bb025b20a406da70cc72ec1f936fd
suyanlong <592938410@qq.com>
2018-03-01 17:25:58 +0800

    update content.go file

--- examples/content.go
+++ examples/content.go
@@ -18,7 +18,26 @@ func main() {
 	//context1()
 	//context2()
 
-	context3()
+	//context3()
+	ExampleWithTimeout()
+
+}
+
+func ExampleWithTimeout() {
+	// Pass a context with a timeout to tell a blocking function that it
+	// should abandon its work after the timeout elapses.
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Println("overslept")
+	case <-ctx.Done():
+		fmt.Println(ctx.Err()) // prints "context deadline exceeded"
+	}
+
+	// Output:
+	// context deadline exceeded
 }
 
 func context3() {
