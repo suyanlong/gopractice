f2c4fd6d2952475abdd7c03e921ed37336ac765c
suyanlong <592938410@qq.com>
2018-03-03 16:09:40 +0800

    fixed context

--- examples/content.go
+++ examples/content.go
@@ -26,7 +26,7 @@ func main() {
 func ExampleWithTimeout() {
 	// Pass a context with a timeout to tell a blocking function that it
 	// should abandon its work after the timeout elapses.
-	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	ctx, cancel := context.WithTimeout(context.Background(), 5000*time.Millisecond)
 	defer cancel()
 
 	select {
