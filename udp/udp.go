d52bfe98e6e09dbe40b1a0610be7f9a331a4e7dc
suyanlong <592938410@qq.com>
2018-03-19 21:35:45 +0800

    udp protocol study

--- udp/udp.go
+++ udp/udp.go
@@ -7,7 +7,9 @@ import (
 )
 
 func sendUdpData() {
-	udpConn, err := net.DialUDP("udp", &net.UDPAddr{IP: net.IPv4zero, Port: 0}, &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 8686})
+	/// laddr 本地地址， raadr远程地址。
+	udpConn, err := net.DialUDP("udp", &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 6786}, &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 8686})
+	fmt.Println(udpConn.LocalAddr().String())
 	if err != nil {
 		fmt.Printf("udp dial err %v", err)
 	}
@@ -16,24 +18,26 @@ func sendUdpData() {
 		fmt.Println("send data")
 		time.Sleep(1 * time.Second)
 	}
-
+	
 	defer udpConn.Close()
-
+	
 }
 
 func LisenterUdpData() {
 	udpConn, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 8686})
+	
 	fmt.Println("listener udp data ")
+	fmt.Println(udpConn.LocalAddr().String())
 	if err != nil {
 		fmt.Printf("udp dial err %v", err)
 	}
-
+	
 	data := make([]byte, 1024)
 	for {
 		udpConn.Read(data)
 		fmt.Println(string(data))
 	}
-
+	
 	fmt.Println("LisenterUdpData end! ")
 	defer udpConn.Close()
 }
