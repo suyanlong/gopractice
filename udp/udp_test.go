df4d0d9362dcfc834da51564c80d6d7b2eb43f3c
suyanlong <592938410@qq.com>
2018-03-22 01:45:15 +0800

    study udp

--- udp/udp_test.go
+++ udp/udp_test.go
@@ -4,11 +4,13 @@ import (
 	"sync"
 	"testing"
 	"time"
+	"fmt"
+	"net"
 )
 
 func TestSendUdpData(t *testing.T) {
 	t.Log("listener")
-
+	
 	wg := sync.WaitGroup{}
 	go func() {
 		wg.Add(1)
@@ -16,8 +18,17 @@ func TestSendUdpData(t *testing.T) {
 		t.Log("listener1212")
 		wg.Done()
 	}()
-
+	
 	time.Sleep(1 * time.Second)
 	LisenterUdpData()
 	wg.Wait()
+}
+
+func TestResolveUdpaddr(t *testing.T) {
+	udpcConn, err := net.ResolveUDPAddr("udp", "127.0.0.1:8088")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	
+	fmt.Println(udpcConn.String())
 }
