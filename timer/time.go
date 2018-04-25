92b858669553f9eb2af68304f6ce61482072a212
yanlong <592938410@qq.com>
2018-04-25 15:49:07 +0800

    study time

--- timer/time.go
+++ timer/time.go
@@ -6,11 +6,13 @@ import (
 )
 
 func main() {
-	k := time.After(time.Second)
+	k := time.After(time.Second * 10)
 	kk := time.Tick(time.Second * 2)
 	for {
 		select {
 		case <-k:
+			fmt.Println(12311111111111)
+		case <-time.After(time.Second * 3):
 			fmt.Println(123)
 		case <-kk:
 			fmt.Println(12323)
